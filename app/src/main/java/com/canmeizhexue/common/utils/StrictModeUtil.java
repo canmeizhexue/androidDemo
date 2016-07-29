8ed1b39a2920f207b58e5c087c12e7ece56ed16c
zengyaping <zengyaping@htmimi.com>
2016-07-29 16:38:11 +0800

    严苛模式工具类

---
+++ app/src/main/java/com/canmeizhexue/common/utils/StrictModeUtil.java
@@ -0,0 +1,36 @@
+package com.canmeizhexue.common.utils;
+
+import android.os.Build;
+import android.os.StrictMode;
+
+/**严苛模式
+ * Created by canmeizhexue on 2016-7-29.
+ */
+public class StrictModeUtil {
+    private StrictModeUtil() {
+        throw new UnsupportedOperationException("this class should not be instantiated");
+    }
+
+    /**
+     *开启严苛模式
+     */
+    public static void enableStrictMode(){
+        StrictMode.ThreadPolicy.Builder threadPolicyBuilder = new StrictMode.ThreadPolicy.Builder();
+        threadPolicyBuilder.detectNetwork().detectDiskWrites().detectDiskReads().detectCustomSlowCalls();
+        threadPolicyBuilder.penaltyLog();
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            threadPolicyBuilder.detectResourceMismatches();
+        }
+        StrictMode.setThreadPolicy(threadPolicyBuilder.build());
+
+        StrictMode.VmPolicy.Builder vmPolicyBuilder = new StrictMode.VmPolicy.Builder();
+        vmPolicyBuilder.detectLeakedSqlLiteObjects().detectLeakedClosableObjects().detectActivityLeaks();
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN){
+            vmPolicyBuilder.detectLeakedRegistrationObjects();
+        }
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR2){
+            vmPolicyBuilder.detectFileUriExposure();
+        }
+        StrictMode.setVmPolicy(vmPolicyBuilder.build());
+    }
+}
---
+++ app/src/main/java/com/canmeizhexue/common/utils/Test.java
@@ -0,0 +1,7 @@
+package com.canmeizhexue.common.utils;
+
+/**
+ * Created by canmeizhexue on 2016-7-29.
+ */
+public class Test {
+}
