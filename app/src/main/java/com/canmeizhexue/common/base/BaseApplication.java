caacac4c64d778c8f02e2f15a2df2ed9ab53f471
zengyaping <zengyaping@htmimi.com>
2016-08-03 10:50:39 +0800

    LeakCanary检测你需要监控对象的内存泄漏

--- app/src/main/java/com/canmeizhexue/common/base/BaseApplication.java
+++ app/src/main/java/com/canmeizhexue/common/base/BaseApplication.java
@@ -4,21 +4,35 @@ import android.app.Application;
 
 import com.canmeizhexue.common.utils.performance.AppBlockCanaryContext;
 import com.github.moduth.blockcanary.BlockCanary;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 /**Application
  * Created by canmeizhexue on 2016-8-3.
  */
 public class BaseApplication extends Application{
     private static BaseApplication baseApplication;
-
+    private RefWatcher refWatcher;
     @Override
     public void onCreate() {
         super.onCreate();
         baseApplication = this;
         //TODO 初始化性能监控组件
         BlockCanary.install(this, new AppBlockCanaryContext()).start();
+        // 使用leakCanary监控那些本该回收的对象, 会返回一个预定义的 RefWatcher，同时也会启用一个 ActivityRefWatcher，用于自动监控调用Activity.onDestroy() 之后泄露的 activity。
+        refWatcher = LeakCanary.install(this);
     }
     public static BaseApplication getApplication(){
         return baseApplication;
+    }
+
+    /**
+     * 监控某个对象是否被回收
+     * @param object 被监控的对象
+     */
+    public void watch(Object object){
+        if(object!=null){
+            refWatcher.watch(object);
+        }
     }
 }
--- app/src/main/java/com/canmeizhexue/common/base/BaseFragment.java
+++ app/src/main/java/com/canmeizhexue/common/base/BaseFragment.java
@@ -6,4 +6,10 @@ import android.app.Fragment;
  * Created by canmeizhexue on 2016-8-3.
  */
 public class BaseFragment extends Fragment{
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // 监控Fragment是否被回收
+        BaseApplication.getApplication().watch(this);
+    }
 }
